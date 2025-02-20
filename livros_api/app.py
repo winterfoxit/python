16a953c6eb36833738b42d881a4ae8bb43452b16
Winter Fox <winterfoxit@gmail.com>
2025-02-20 16:53:00 -0300

    final

--- livros_api/app.py
+++ livros_api/app.py
@@ -25,4 +25,27 @@ livros = [
 def obter_livros():
     return jsonify(livros)
 
-app.run(port=5000, host='localhost',debug=True)
+# Consultar por id
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+
+# Editar lista de livros
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+# Criar livro
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(novo_livro)
+
+app.run(port=5000, host='localhost', debug=True)
